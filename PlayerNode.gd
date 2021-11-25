f7e3b4af4ca4d332118993c05097e7380e7bfda9
attowls <planting31@ajou.ac.kr>
2021-11-26 03:00:30 +0900

    move to return point when player goes out of screen

--- PlayerNode.gd
+++ PlayerNode.gd
@@ -5,7 +5,7 @@ const WALK_SPEED = 100
 const JUMP_FORCE = 100
 
 var velocity = Vector2()
-
+var return_pos = Vector2()
 
 func _physics_process(delta):
 	
@@ -22,6 +22,21 @@ func _physics_process(delta):
 
 	else:
 		velocity.x = 0
+
+
+	#check return point when player goes out of the screen
+	if is_on_floor():
+		if Input.is_action_pressed("ui_left"):
+			return_pos = Vector2(self.get_position().x+30, self.get_position().y-10)
+		 
+		elif Input.is_action_pressed("ui_right"):
+			return_pos = Vector2(self.get_position().x-30, self.get_position().y-10)
+		else:
+			return_pos = Vector2(self.get_position().x, self.get_position().y-10)
+			
+	# go to return point when player goes out of the screen       
+	if self.get_position().y > get_viewport_rect().size.y:
+		self.set_position(return_pos)
 
 	
 	velocity = move_and_slide(velocity, Vector2.UP)
