148a27512e1217ce9853fc936347abd9da761566
nori-sama <lordseaweed@gmail.com>
2021-11-26 10:41:25 +0900

    Changed stuff during class

--- PlayerNode.gd
+++ PlayerNode.gd
@@ -2,7 +2,9 @@ extends KinematicBody2D
 
 const gravity = 200.0
 const WALK_SPEED = 100
-const JUMP_FORCE = 100
+const JUMP_FORCE = 125
+
+var facing_left = true
 
 var velocity = Vector2()
 var return_pos = Vector2()
@@ -11,14 +13,21 @@ func _physics_process(delta):
 	
 	velocity.y += delta * gravity
 	
+	if facing_left == true:
+		$Sprite.scale.x = 1
+	else:
+		$Sprite.scale.x = -1
+	
 	if Input.is_action_pressed("ui_select") and is_on_floor():
 		velocity.y = -JUMP_FORCE
 
 	if Input.is_action_pressed("ui_left"):
 		velocity.x = -WALK_SPEED
+		facing_left = true
 
 	elif Input.is_action_pressed("ui_right"):
 		velocity.x =  WALK_SPEED
+		facing_left = false
 
 	else:
 		velocity.x = 0
