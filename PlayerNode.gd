a12699b8ef99fa7014da5876af03117db07e51ae
attowls <planting31@ajou.ac.kr>
2021-12-03 01:42:21 +0900

    change player image and modify animation of player

--- PlayerNode.gd
+++ PlayerNode.gd
@@ -6,35 +6,46 @@ const gravity = 200.0
 const WALK_SPEED = 100
 const JUMP_FORCE = 125
 
-var facing_left = true
-
 var velocity = Vector2()
 var return_pos = Vector2()
 
+onready var animationPlayer = $AnimationPlayer
+
 var life = 3;
+
 
 func _physics_process(delta):
 	
 	velocity.y += delta * gravity
 	
-	if facing_left == true:
-		$Sprite.scale.x = 1
-	else:
-		$Sprite.scale.x = -1
-	
 	if Input.is_action_pressed("ui_select") and is_on_floor():
 		velocity.y = -JUMP_FORCE
 
+
 	if Input.is_action_pressed("ui_left"):
-		velocity.x = -WALK_SPEED
-		facing_left = true
+		if is_on_floor():
+			animationPlayer.play("WalkLeft")
+			velocity.x = -WALK_SPEED
+		else:
+			animationPlayer.play("JumpLeft")
+			velocity.x = -WALK_SPEED
+			
 
 	elif Input.is_action_pressed("ui_right"):
-		velocity.x =  WALK_SPEED
-		facing_left = false
+		if is_on_floor():
+			animationPlayer.play("WalkRight")
+			velocity.x =  WALK_SPEED
+		else:
+			animationPlayer.play("JumpRight")
+			velocity.x = WALK_SPEED
 
 	else:
-		velocity.x = 0
+		if is_on_floor():
+			animationPlayer.play("idle")
+			velocity.x = 0
+		else:
+			animationPlayer.play("Jump")
+			velocity.x = 0
 
 
 	#check return point when player goes out of the screen
