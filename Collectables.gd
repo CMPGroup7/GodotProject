491adcf25c103103588f6bf8996cf8897a9824b6
attowls <planting31@ajou.ac.kr>
2021-12-03 02:08:05 +0900

    apply jjokji animation

--- Collectables.gd
+++ Collectables.gd
@@ -2,7 +2,11 @@ extends Area2D
 class_name Collectables
 var collectCount = 0
 
+onready var animationPlayer = $AnimationPlayer
+
 func _on_Jjokji_body_entered(body):
+	animationPlayer.play("OpenJjokji")
+	yield(animationPlayer, "animation_finished")
 	self.queue_free()
 	collectCount = collectCount + 1
 	
