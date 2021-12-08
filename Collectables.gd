9a037b9d4c1bbf4f1ace291ea3a95dfa201af50c
KimmYejin <qjarnek@ajou.ac.kr>
2021-12-08 12:14:57 +0900

    Alphabet visibility

--- Collectables.gd
+++ Collectables.gd
@@ -1,17 +1,15 @@
 extends Area2D
 class_name Collectables
-var collectCount = 0
 
 onready var animationPlayer = $AnimationPlayer
-
+onready var global_vars = get_node("/root/GlobalVariables")
 func _on_Jjokji_body_entered(body):
 	animationPlayer.play("OpenJjokji")
 	yield(animationPlayer, "animation_finished")
 	self.queue_free()
-
-	collectCount += 1
-	print (collectCount)
-	match[collectCount]:
+	global_vars.collected += 1 
+	print(global_vars.collected)
+	match[global_vars.collected]:
 		[1]: get_node("/root/World/CameraRoot/UI_Group/TextGroup/P").visible = true
 		[2]: get_node("/root/World/CameraRoot/UI_Group/TextGroup/R").visible = true
 		[3]: get_node("/root/World/CameraRoot/UI_Group/TextGroup/E").visible = true
