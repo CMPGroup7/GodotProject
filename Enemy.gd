f8ed89f03589cdb17f548f11a621d9e941fb87ce
attowls <planting31@ajou.ac.kr>
2021-12-08 12:54:43 +0900

    add enemy movement

--- Enemy.gd
+++ Enemy.gd
@@ -1,9 +1,36 @@
 extends KinematicBody2D
 
-var velocity = Vector2()
-const gravity = 200.0
+var enemy_velocity = Vector2()
+const enemy_gravity = 200.0
+var WALK_SPEED = 20
+var change_direction = false
+
 onready var animationPlayer = $AnimationPlayer
+onready var rayCast = get_node("RayCast2D")
+
 func _physics_process(delta):
+	
 	animationPlayer.play("enemy_walk")
-	velocity.y += delta * gravity
-	move_and_collide(velocity)
+	
+	enemy_velocity.y += delta * enemy_gravity
+	enemy_velocity.x = -WALK_SPEED
+	
+	move_and_slide(enemy_velocity)
+	
+	try_interact()
+		
+func try_interact():
+
+	var collider = rayCast.get_collider()
+
+	if rayCast.is_colliding():
+		if collider is TileMap:
+			print("coll!")
+		if change_direction == true:
+			change_direction = false
+	else :
+
+		if change_direction == false:
+			change_direction = true
+			self.scale.x *= -1
+			WALK_SPEED = -WALK_SPEED
